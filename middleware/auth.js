896d5094a480f4a94dbb4c608db8ba8de4448919
muradhosenrafi <muradhosenrafi@gmail.com>
2026-09-25 22:25:20 +0600

    add jwt

--- middleware/auth.js
+++ middleware/auth.js
@@ -5,5 +5,11 @@ const token = res.cookies?.token ||
 (req.headers.authMiddleware && req.headers.authorization.split("")[1])
 if(!token){
     return res.status(401).json({success:false, message:"Token Missing"})
+}try{
+    const decoded = jwt.verify(token,process.env.JWT_SECRET)
+
+}catch(error){
+
 }
+
 }
